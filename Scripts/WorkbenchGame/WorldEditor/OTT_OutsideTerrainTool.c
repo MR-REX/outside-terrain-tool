276f7c943272323d172c0c4975d167112cb6be91
MR.REX <mrrex.main@gmail.com>
2024-07-30 00:27:46 +0300

    Update OTT_OutsideTerrainTool.c

--- Scripts/WorkbenchGame/WorldEditor/OTT_OutsideTerrainTool.c
+++ Scripts/WorkbenchGame/WorldEditor/OTT_OutsideTerrainTool.c
@@ -3,19 +3,192 @@
 [WorkbenchToolAttribute(name: "Outside Terrain Tool", awesomeFontCode: 0xF1FE)]
 class OTT_OutsideTerrainTool : WorldEditorTool
 {
-	[Attribute(category: "Noise Modifier", defvalue: "1", uiwidget: UIWidgets.CheckBox, desc: "Enable noise modifier for all outside terrain heightmaps")]
+	// Category: General
+	
+	[
+		Attribute
+		(
+			category: "General",
+			desc: "Outside terrain generation type",
+			uiwidget: UIWidgets.ComboBox,
+			enums: ParamEnumArray.FromEnum(OTT_EOutsideTerrainGeneratorTypes),
+			//defvalue: OTT_EOutsideTerrainGeneratorTypes..ToString()
+		)
+	]
+	protected OTT_EOutsideTerrainGeneratorTypes m_eOutsideTerrainGeneratorType;
+	
+	[
+		Attribute
+		(
+			category: "General",
+			desc: "Size of outside terrain",
+			uiwidget: UIWidgets.ComboBox,
+			enums: ParamEnumArray.FromEnum(OTT_EOutsideTerrainSize),
+			defvalue: OTT_EOutsideTerrainSize.Normal.ToString()
+		)
+	]
+	protected OTT_EOutsideTerrainSize m_eOutsideTerrainSize;
+	
+	[
+		Attribute
+		(
+			category: "General",
+			desc: "Quality of outside terrain",
+			uiwidget: UIWidgets.ComboBox,
+			enums: ParamEnumArray.FromEnum(OTT_EOutsideTerrainQuality),
+			defvalue: OTT_EOutsideTerrainQuality.High.ToString()
+		)
+	]
+	protected OTT_EOutsideTerrainQuality m_eOutsideTerrainQuality;
+	
+	// Category: Physics
+	
+	[
+		Attribute
+		(
+			category: "Physics",
+			desc: "Enable physics body creation for outside terrain chunks",
+			uiwidget: UIWidgets.CheckBox,
+			defvalue: "1"
+		)
+	]
+	protected bool m_bEnableOutsideTerrainPhysics;
+	
+	[
+		Attribute
+		(
+			category: "Physics",
+			desc: "Physics layer preset for outside terrain chunks",
+			uiwidget: UIWidgets.ComboBox,
+			enums: ParamEnumArray.FromEnum(EPhysicsLayerPresets),
+			defvalue: EPhysicsLayerPresets.Terrain.ToString()
+		)
+	]
+	protected EPhysicsLayerPresets m_ePhysicsLayerPreset;
+	
+	// Category: Noise Modifier
+	
+	[
+		Attribute
+		(
+			category: "Noise Modifier",
+			desc: "Enable noise modifier for all outside terrain heightmaps",
+			uiwidget: UIWidgets.CheckBox,
+			defvalue: "1"
+		)
+	]
 	protected bool m_bEnableNoiseModifier;
 	
-	[Attribute(category: "Noise Modifier", defvalue: "0", uiwidget: UIWidgets.ComboBox, enums: ParamEnumArray.FromEnum(OTT_ENoiseAlgorithm))]
+	[
+		Attribute
+		(
+			category: "Noise Modifier",
+			uiwidget: UIWidgets.ComboBox,
+			enums: ParamEnumArray.FromEnum(OTT_ENoiseAlgorithm),
+			defvalue: "0"
+		)
+	]
 	protected OTT_ENoiseAlgorithm m_eNoiseAlgorithm;
 	
-	[Attribute(category: "Noise Modifier", defvalue: "Hello, outside terrain!", desc: "String will be converted to numeric seed for noise modifier")]
-	protected string m_sNoiseModifierSeed;
+	[
+		Attribute
+		(
+			category: "Noise Modifier",
+			desc: "Seed for noise modifier",
+			defvalue: "Hello, outside terrain!"
+		)
+	]
+	protected string m_sNoiseSeed;
+	
+	// Category: Materials
+	
+	[
+		Attribute
+		(
+			category: "Materials",
+			desc: "Material of outside terrain chunks",
+			uiwidget: UIWidgets.ResourcePickerThumbnail,
+			params: "emat",
+			defvalue: "{FCDCBAF69074F24C}Common/Textures/OutsideTerrainCore/DefaultMaterial.emat"
+		)
+	]
+	protected ResourceName m_ChunksMaterial;
+	
+	// Category: Layer
+	
+	[
+		Attribute
+		(
+			category: "Layer",
+			desc: "Template for name of outside terrain chunks layer",
+			defvalue: "OutsideTerrain"
+		)
+	]
+	protected string m_sLayerNameTemplate;
+	
+	[
+		Attribute
+		(
+			category: "Layer",
+			desc: "Template for name of outside terrain chunk entity",
+			defvalue: "OutsideTerrainChunk_%1"
+		)
+	]
+	protected string m_sEntityNameTemplate;
+	
+	// Category: Tool
+	
+	[
+		Attribute
+		(
+			category: "Tool",
+			desc: "Automatically delete an existing outside terrain layer before generating a new one",
+			defvalue: "1"
+		)
+	]
+	protected bool m_bEnableAutoLayerCleanup;
+	
+	// Button: Generate
 	
 	[ButtonAttribute("Generate")]
 	void Generate()
 	{
+		OTT_OutsideTerrainGeneratorOptions generatorOptions = new OTT_OutsideTerrainGeneratorOptions(
+			size: m_eOutsideTerrainSize,
+			quality: m_eOutsideTerrainQuality,
+			enablePhysics: m_bEnableOutsideTerrainPhysics,
+			physicsLayerPreset: m_ePhysicsLayerPreset,
+			chunksMaterial: m_ChunksMaterial,
+			layerNameTemplate: m_sLayerNameTemplate,
+			entityNameTemplate: m_sEntityNameTemplate
+		);
 		
+		OTT_OutsideTerrainGenerator generator = OTT_OutsideTerrainGeneratorFactory.Create(
+			type: m_eOutsideTerrainGeneratorType,
+			options: generatorOptions
+		);
+		
+		if (!generator)
+		{
+			Workbench.Dialog("Failed to initialize outside terrain generator", "Failed to create an instance of generator with selected outside terrain type.");
+			return;
+		}
+		
+		OTT_NoiseModifier noiseModifier;
+		
+		if (m_bEnableNoiseModifier)
+		{
+			noiseModifier = new OTT_NoiseModifier(m_eNoiseAlgorithm);
+			noiseModifier.SetSeed(m_sNoiseSeed);
+		}
+		
+		bool success = generator.Process(noiseModifier);
+		
+		if (!success)
+		{
+			Workbench.Dialog("Failed to generate outside terrain", "Outside terrain generator reported that outside terrain generation process was not fully completed.");
+			return;
+		}
 	}
 }
 
