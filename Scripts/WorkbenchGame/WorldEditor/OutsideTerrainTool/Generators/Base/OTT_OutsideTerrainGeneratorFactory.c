560198a20f4c25208d9ce1eef89064b8703e1b35
MR.REX <mrrex.main@gmail.com>
2024-07-30 00:27:43 +0300

    Outside terrain generator factory

---
+++ Scripts/WorkbenchGame/WorldEditor/OutsideTerrainTool/Generators/Base/OTT_OutsideTerrainGeneratorFactory.c
@@ -0,0 +1,16 @@
+#ifdef WORKBENCH
+
+enum OTT_EOutsideTerrainGeneratorTypes
+{
+
+}
+
+class OTT_OutsideTerrainGeneratorFactory
+{
+	static OTT_OutsideTerrainGenerator Create(OTT_EOutsideTerrainGeneratorTypes type, OTT_OutsideTerrainGeneratorOptions options)
+	{
+		return null;
+	}
+}
+
+#endif
