e09d63e09d03e4d0a5eb23f0a49eb4a562603189
MR.REX <mrrex.main@gmail.com>
2024-08-04 21:12:05 +0300

    Update OTT_SimpleOutsideTerrainGenerator.c

--- Scripts/WorkbenchGame/WorldEditor/OutsideTerrainTool/Generators/OTT_SimpleOutsideTerrainGenerator.c
+++ Scripts/WorkbenchGame/WorldEditor/OutsideTerrainTool/Generators/OTT_SimpleOutsideTerrainGenerator.c
@@ -36,12 +36,34 @@ class OTT_SimpleOutsideTerrainGenerator : OTT_OutsideTerrainGenerator
 		return 0;
 	}
 	
+	protected float GetChunksDepthMultiplier()
+	{
+		OTT_EOutsideTerrainSize size = m_Options.GetSize();
+		
+		switch (size)
+		{
+			case OTT_EOutsideTerrainSize.Maximum:
+				return 1;
+			case OTT_EOutsideTerrainSize.Normal:
+				return 0.5;
+			case OTT_EOutsideTerrainSize.Minimal:
+				return 0.25;
+		}
+		
+		return 0;
+	}
+	
 	override protected bool Process()
 	{
-		// Getting terrain heightmap
+		// Getting chunks configuration
 		
 		int chunkResolution = GetChunkResolution();
 		int chunksCount = GetChunksCount();
+		
+		if (chunkResolution == 0 || chunksCount == 0)
+			return false;
+		
+		// Getting terrain heightmap
 		
 		int terrainHeightmapResolution = chunkResolution * chunksCount;
 		
@@ -52,6 +74,14 @@ class OTT_SimpleOutsideTerrainGenerator : OTT_OutsideTerrainGenerator
 			width: terrainHeightmapResolution,
 			height: terrainHeightmapResolution
 		);
+		
+		// Getting chunks depth
+		
+		float chunksDepthMultiplier = GetChunksDepthMultiplier();
+		int chunksDepth = Math.Ceil(chunksCount * chunksDepthMultiplier);
+		
+		if (chunksDepth == 0 || chunksDepth > chunksCount)
+			return false;
 		
 		// Joining chunks border heights on terrain heightmap
 		
@@ -84,10 +114,6 @@ class OTT_SimpleOutsideTerrainGenerator : OTT_OutsideTerrainGenerator
 		
 		vector chunkSize = {chunkWidth, 0, chunkHeight};
 		
-		// Calculating plane size
-		
-		float planeSize = chunksCount;
-		
 		// Initializing temporary variables
 		
 		int x, y;
@@ -101,7 +127,7 @@ class OTT_SimpleOutsideTerrainGenerator : OTT_OutsideTerrainGenerator
 		
 		for (int i = 0; i < chunksCount; i++)
 		{
-			for (int j = 0; j < chunksCount; j++)
+			for (int j = 0; j < chunksDepth; j++)
 			{
 				x = chunkResolution * j;
 				y = chunkResolution * i;
@@ -111,7 +137,7 @@ class OTT_SimpleOutsideTerrainGenerator : OTT_OutsideTerrainGenerator
 				position = {
 					m_vTerrainMins[0] + (chunkWidth / 2) + (chunkWidth * i),
 					m_vTerrainMins[1] - 8,
-					m_vTerrainMins[2] + (chunkHeight * planeSize) + (chunkHeight / 2) + (chunkHeight * j)
+					m_vTerrainMins[2] + (chunkHeight * chunksCount) + (chunkHeight / 2) + (chunkHeight * j)
 				};
 				
 				CreateChunk(position, vector.Zero, chunkSize, chunkHeightmap, true);
