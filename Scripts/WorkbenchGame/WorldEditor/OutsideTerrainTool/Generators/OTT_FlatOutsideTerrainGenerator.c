b9bf1ab584b3b20c97047b4ce95023f9e7876424
MR.REX <mrrex.main@gmail.com>
2024-11-25 22:00:49 +0300

    Chunks generation for Flat generator

--- Scripts/WorkbenchGame/WorldEditor/OutsideTerrainTool/Generators/OTT_FlatOutsideTerrainGenerator.c
+++ Scripts/WorkbenchGame/WorldEditor/OutsideTerrainTool/Generators/OTT_FlatOutsideTerrainGenerator.c
@@ -80,6 +80,250 @@ class OTT_FlatOutsideTerrainGenerator : OTT_OutsideTerrainGenerator
 	
 	protected bool processChunks()
 	{
+		// Getting context options
+		
+		OTT_OutsideTerrainContextOptions contextOptions = m_Options.GetContextOptions();
+		
+		// Getting terrain data
+		
+		vector terrainMins = m_Terrain.GetMins();
+		vector terrainSize = m_Terrain.GetSize();
+		
+		// Calculating outside terrain position
+		
+		vector outsideTerrainPosition = {terrainMins[0], 0, terrainMins[2]};
+		
+		// Getting chunks properties
+		
+		int chunkResolution = GetChunkResolution();
+		int chunksCount = GetChunksCount();
+		
+		// Getting chunk size
+		
+		float chunkSize = GetChunkSize();
+		vector chunkSizes = {chunkSize, 0, chunkSize};
+		
+		// Should enable physics for chunks?
+		
+		bool shouldEnablePhysics = m_Options.GetPhysicsType() == OTT_EOutsideTerrainPhysicsType.Full;
+		
+		// Getting generation options
+		
+		float chunksDepthOffset = m_Options.GetDepthOffset();
+		
+		// Reusable variables
+		
+		vector chunkPosition;
+		bool shouldCreate;
+		
+		// Creating chunks for North side
+		
+		m_Manager.SetChunksPrefix("North");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] / 2, terrainSize[2] - 1) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.North);
+		
+		if (shouldCreate)
+		{
+			for (int i = 0; i < chunksCount; i++)
+			{
+				chunkPosition = outsideTerrainPosition + {
+					(chunkSize / 2) + (chunkSize * i),
+					0,
+					terrainSize[2] + (chunkSize / 2) + chunksDepthOffset
+				};
+				
+				CreateChunk(
+					position: chunkPosition,
+					angles: vector.Zero,
+					size: chunkSizes,
+					heightmap: {{0, 0}, {0, 0}},
+					enablePhysics: shouldEnablePhysics
+				);
+			}
+		}
+		
+		// Creating chunks for South side
+		
+		m_Manager.SetChunksPrefix("South");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] / 2, 0) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.South);
+		
+		if (shouldCreate)
+		{
+			for (int i = 0; i < chunksCount; i++)
+			{
+				chunkPosition = outsideTerrainPosition + {
+					(chunkSize / 2) + (chunkSize * i),
+					0,
+					-(chunkSize / 2) - chunksDepthOffset
+				};
+				
+				CreateChunk(
+					position: chunkPosition,
+					angles: vector.Zero,
+					size: chunkSizes,
+					heightmap: {{0, 0}, {0, 0}},
+					enablePhysics: shouldEnablePhysics
+				);
+			}
+		}
+		
+		// Creating chunks for West side
+		
+		m_Manager.SetChunksPrefix("West");
+		
+		shouldCreate = ShouldProcessSide(0, terrainSize[2] / 2) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.West);
+		
+		if (shouldCreate)
+		{
+			for (int i = 0; i < chunksCount; i++)
+			{
+				chunkPosition = outsideTerrainPosition + {
+					-(chunkSize / 2) - chunksDepthOffset,
+					0,
+					(chunkSize / 2) + (chunkSize * i)
+				};
+				
+				CreateChunk(
+					position: chunkPosition,
+					angles: vector.Zero,
+					size: chunkSizes,
+					heightmap: {{0, 0}, {0, 0}},
+					enablePhysics: shouldEnablePhysics
+				);
+			}
+		}
+		
+		// Creating chunks for East side
+		
+		m_Manager.SetChunksPrefix("East");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] - 1, terrainSize[2] / 2) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.East);
+		
+		if (shouldCreate)
+		{
+			for (int i = 0; i < chunksCount; i++)
+			{
+				chunkPosition = outsideTerrainPosition + {
+					terrainSize[0] + (chunkSize / 2) + chunksDepthOffset,
+					0,
+					(chunkSize / 2) + (chunkSize * i)
+				};
+				
+				CreateChunk(
+					position: chunkPosition,
+					angles: vector.Zero,
+					size: chunkSizes,
+					heightmap: {{0, 0}, {0, 0}},
+					enablePhysics: shouldEnablePhysics
+				);
+			}
+		}
+		
+		// Create chunk at North-West corner
+		
+		m_Manager.SetChunksPrefix("NorthWest");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] / 2, terrainSize[2] - 1) &&
+					   ShouldProcessSide(0, terrainSize[2] / 2) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.NorthWest);
+		
+		if (shouldCreate)
+		{
+			chunkPosition = outsideTerrainPosition + {
+				-(chunkSize / 2) - chunksDepthOffset,
+				0,
+				terrainSize[2] + (chunkSize / 2) + chunksDepthOffset
+			};
+			
+			CreateChunk(
+				position: chunkPosition,
+				angles: vector.Zero,
+				size: chunkSizes,
+				heightmap: {{0, 0}, {0, 0}},
+				enablePhysics: shouldEnablePhysics
+			);
+		}
+		
+		// Create chunk at North-East corner
+		
+		m_Manager.SetChunksPrefix("NorthEast");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] / 2, terrainSize[2] - 1) &&
+					   ShouldProcessSide(terrainSize[0] - 1, terrainSize[2] / 2) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.NorthEast);
+		
+		if (shouldCreate)
+		{
+			chunkPosition = outsideTerrainPosition + {
+				terrainSize[0] + (chunkSize / 2) + chunksDepthOffset,
+				0,
+				terrainSize[2] + (chunkSize / 2) + chunksDepthOffset
+			};
+			
+			CreateChunk(
+				position: chunkPosition,
+				angles: vector.Zero,
+				size: chunkSizes,
+				heightmap: {{0, 0}, {0, 0}},
+				enablePhysics: shouldEnablePhysics
+			);
+		}
+		
+		// Create chunk at South-West corner
+		
+		m_Manager.SetChunksPrefix("SouthWest");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] / 2, 0) &&
+					   ShouldProcessSide(0, terrainSize[2] / 2) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.SouthWest);
+		
+		if (shouldCreate)
+		{
+			chunkPosition = outsideTerrainPosition + {
+				-(chunkSize / 2) - chunksDepthOffset,
+				0,
+				-(chunkSize / 2) - chunksDepthOffset
+			};
+			
+			CreateChunk(
+				position: chunkPosition,
+				angles: vector.Zero,
+				size: chunkSizes,
+				heightmap: {{0, 0}, {0, 0}},
+				enablePhysics: shouldEnablePhysics
+			);
+		}
+		
+		// Create chunk at South-East corner
+		
+		m_Manager.SetChunksPrefix("SouthEast");
+		
+		shouldCreate = ShouldProcessSide(terrainSize[0] / 2, 0) &&
+					   ShouldProcessSide(terrainSize[0] - 1, terrainSize[2] / 2) &&
+					   !contextOptions.ShouldIgnoreDirection(OTT_CardinalDirections.SouthEast);
+		
+		if (shouldCreate)
+		{
+			chunkPosition = outsideTerrainPosition + {
+				terrainSize[0] + (chunkSize / 2) + chunksDepthOffset,
+				0,
+				-(chunkSize / 2) + chunksDepthOffset
+			};
+			
+			CreateChunk(
+				position: chunkPosition,
+				angles: vector.Zero,
+				size: chunkSizes,
+				heightmap: {{0, 0}, {0, 0}},
+				enablePhysics: shouldEnablePhysics
+			);
+		}
+		
 		return true;
 	}
 	
